ac9bcf8307f5ae3ac252f1b1d4a6a43cd3861c3a
LiuXinhao <2779019980@qq.com>
2026-05-15 18:19:01 +0800

    user模块微服务拆分

---
+++ user-service/src/main/java/com/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "com.api.client")
+@MapperScan("com.user.mapper")
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
