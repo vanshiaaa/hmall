c87408383bd18e3c7c65c941ba55d82d4ec69f92
LiuXinhao <2779019980@qq.com>
2026-05-17 18:52:37 +0800

    网关登录校验功能（传递用户信息，微服务之间调用使用openfeign传递信息）

--- trade-service/src/main/java/com/trade/TradeApplication.java
+++ trade-service/src/main/java/com/trade/TradeApplication.java
@@ -1,11 +1,12 @@
 package com.trade;
 
+import com.api.config.DefaltFeignLevel;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.api.client")
+@EnableFeignClients(basePackages = "com.api.client", defaultConfiguration = DefaltFeignLevel.class)
 @MapperScan("com.trade.mapper")
 @SpringBootApplication
 public class TradeApplication {
