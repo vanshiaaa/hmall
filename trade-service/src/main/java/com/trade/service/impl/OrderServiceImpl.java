f2c4f4f32a464b3e8875731efe7a548a2d0277af
LiuXinhao <2779019980@qq.com>
2026-05-18 20:41:53 +0800

    集成seata

--- trade-service/src/main/java/com/trade/service/impl/OrderServiceImpl.java
+++ trade-service/src/main/java/com/trade/service/impl/OrderServiceImpl.java
@@ -15,6 +15,7 @@ import com.trade.domain.po.OrderDetail;
 import com.trade.mapper.OrderMapper;
 import com.trade.service.IOrderDetailService;
 import com.trade.service.IOrderService;
+import io.seata.spring.annotation.GlobalTransactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,7 +46,7 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     private final CartClient cartClient;
     private final ItemClient itemClient;
     @Override
-    @Transactional
+    @GlobalTransactional
     public Long createOrder(OrderFormDTO orderFormDTO) {
         // 1.订单数据
         Order order = new Order();
