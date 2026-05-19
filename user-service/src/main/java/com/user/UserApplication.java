4192fe0012c679738157c839f4e17f721620e85e
LiuXinhao <2779019980@qq.com>
2026-05-19 22:42:05 +0800

    (rabbitmq改造)最终版！！！！！！！！

--- user-service/src/main/java/com/user/UserApplication.java
+++ user-service/src/main/java/com/user/UserApplication.java
@@ -1,11 +1,13 @@
 package com.user;
 
+import com.api.config.DefaltFeignLevel;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.api.client")
+//@EnableFeignClients(basePackages = "com.api.client")
+@EnableFeignClients(basePackages = "com.api.client", defaultConfiguration = DefaltFeignLevel.class)
 @MapperScan("com.user.mapper")
 @SpringBootApplication
 public class UserApplication {
